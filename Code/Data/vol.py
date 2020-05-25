dd570ca1167ada3a1042d766cf4caec56efab22f
Gregory <greggiordano12@gmail.com>
2020-05-25 10:27:01 -0400

    Recreated what was lost last time. Volatilty distribution plots

--- Code/Data/vol.py
+++ Code/Data/vol.py
@@ -1,35 +1,59 @@
 import pandas as pd
 import numpy as np
 import pandas_datareader.data as pdr
+import matplotlib.pyplot as plt
 
-temp_df = pdr.DataReader("^VIX","yahoo", '2010-01-01', '2020-03-17')
+vix_df = pdr.DataReader("^VIX","yahoo", '2007-01-01')
+vix_close = vix_df.Close
+vix_close_arr = np.array(vix_close)
 
+# Visualize the VIX time series with lines at $30 and $20 to separate high and low volatilty regimes. Visually appears that above 30 is high vol, between 20
+# and 30 is med vol, and below 20 is low vol
+hor_30 = [30 for i in vix_close]
+hor_20 = [20 for i in vix_close]
+plt.plot(vix_close)
+plt.plot(vix_df.index, hor_30)
+plt.plot(vix_df.index, hor_20)
+plt.xlabel("Year")
+plt.ylabel("Closing Price")
+plt.title("Time Series of VIX Close 2007-Present")
+plt.show()
 
-print(temp_df.head())
+# Histogram to further visualize the distribution
+plt.hist(vix_close_arr, bins = 40)
+plt.xlabel("Closing Price")
+plt.ylabel("Number of Days")
+plt.title("Distribution of VIX Closing Price")
+plt.show()
+
 
 
 # We first can separate the vix data into the 3 regimes: low vol, med vol high vol
-# Below 20 is low vol
-# 20 - 30 is med vol
-# Above 30 is high vol
+# Below 20 is low vol (use number 1 to represent that)
+# 20 - 30 is med vol (use number 2 to represent)
+# Above 30 is high vol (use number 3 to represent)
 
-dates = temp_df.index
 
-# Assigning the dates to a regime
-low_vol = []
-med_vol = []
-high_vol = []
-for i in range(len(temp_df)):
-    if(temp_df.Close[i] < 20):
-        low_vol.append(dates[i])
-    if(temp_df.Close[i] > 30):
-        high_vol.append(dates[i])
+# Assigning the dates to a regime and creating regime dataset
+dates = vix_df.index
+low_vol_dates, med_vol_dates, high_vol_dates = [], [], []
+vol_regime = []
+count = 0
+for i in range(len(vix_close)):
+    count +=1
+    if(vix_df.Close[i] < 20):
+        low_vol_dates.append(dates[i])
+        vol_regime.append(1)
+    elif(vix_df.Close[i] > 30):
+        high_vol_dates.append(dates[i])
+        vol_regime.append(3)
     else:
-        med_vol.append(dates[i])
+        med_vol_dates.append(dates[i])
+        vol_regime.append(2)
 
-print(len(low_vol))
-print(len(med_vol))
-print(len(high_vol))
 
-print("Hey Greg how are you")
-print("Hello")
+# Create volatility regime distribution
+plt.bar(["Low Vol", "Med Vol", "High Vol"], [len(low_vol_dates), len(med_vol_dates), len(high_vol_dates)])
+plt.xlabel("Volatility Regime")
+plt.ylabel("Number of Days")
+plt.title("Volatility Regime Distribution")
