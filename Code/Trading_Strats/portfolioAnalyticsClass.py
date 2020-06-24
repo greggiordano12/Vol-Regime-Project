4465fbc9a4427a6c5213490a7e59552b8b4d0199
Greg Giordano <greggiordano12@gmail.com>
2020-06-23 20:03:29 -0400

    Updates

---
+++ Code/Trading_Strats/portfolioAnalyticsClass.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import pandas_datareader.data as pdr
+import scipy.optimize as sco
+from Code.Data.Inputs import volClass
+from Code.Data import rfClass
+from Code.Trading_Strats import portfolioclass
+
+class portfolioAnalytics:
+    def __init__(self, )
